dc9db45300003dce44335f00a65293f43ef4adbe
Shubhash Singh <subhashsingh2059@gmail.com>
2025-03-13 20:39:18 +0530

    Updated all previous file

---
+++ hosts/default/variables.nix
@@ -0,0 +1,27 @@
+{
+  # Git Configuration ( For Pulling Software Repos )
+  gitUsername = "Tyler Kelley";
+  gitEmail = "tylerzanekelley@gmail.com";
+
+  # Hyprland Settings
+  extraMonitorSettings = "";
+
+  # Waybar Settings
+  clock24h = false;
+
+  # Program Options
+  browser = "firefox"; # Set Default Browser (google-chrome-stable for google-chrome)
+  terminal = "kitty"; # Set Default System Terminal
+  keyboardLayout = "us";
+  consoleKeyMap = "us";
+
+  # For Nvidia Prime support
+  intelID = "PCI:1:0:0";
+  nvidiaID = "PCI:0:2:0";
+
+  # Enable NFS
+  enableNFS = true;
+
+  # Enable Printing Support
+  printEnable = false;
+}
---
+++ modules/home/default.nix
@@ -0,0 +1,29 @@
+{...}: {
+  imports = [
+    ./bash.nix
+    ./bat.nix
+    ./btop.nix
+    ./cava.nix
+    ./emoji.nix
+    ./fastfetch
+    ./gh.nix
+    ./ghostty.nix
+    ./git.nix
+    ./gtk.nix
+    ./hyprland
+    ./kitty.nix
+    ./lang.nix
+    ./nvf.nix
+    ./rofi
+    ./qt.nix
+    ./scripts
+    ./starship.nix
+    ./stylix.nix
+    ./swaync.nix
+    ./virtmanager.nix
+    ./waybar.nix
+    ./wlogout
+    ./xdg.nix
+    ./zsh.nix
+  ];
+}
